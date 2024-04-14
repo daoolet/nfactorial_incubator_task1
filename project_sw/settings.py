8ba5ae66cddfd543e8187a460be85a26b01c2d37
Daoolet <dauletkhabidullin@gmail.com>
2024-04-14 20:26:54 +0500

    re-created project

--- project_sw/settings.py
+++ project_sw/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nxhey&h%@3k7jc5n6t(x&smn8qyz%10mai%5afauav1zlnoc4$'
+SECRET_KEY = 'django-insecure-xc1v83i4@p#*0du@dsd#7^x*6^fqo&(alem(m@0ljql-6*u!n@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_sw.apps.AppSwConfig',
 ]
 
 MIDDLEWARE = [
