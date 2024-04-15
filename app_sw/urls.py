447aedc92c129fd13e5383c23d15d3aa58493fce
Daoolet <dauletkhabidullin@gmail.com>
2024-04-15 22:16:50 +0500

    added urls and views

--- app_sw/urls.py
+++ app_sw/urls.py
@@ -6,6 +6,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("planets/", views.get_planets, name="planets"),
+    path("planets/<int:id>", views.planet_detail, name="planet_detail"),
     path("people/", views.get_people, name="people"),
+    path("people/<int:id>", views.people_detail, name="people_detail"),
     path("starships/", views.get_starships, name="starships"),
+    path("starships/<int:id>", views.starship_detail, name="starship_detail"),
 ]
--- app_sw/views.py
+++ app_sw/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 import requests
-
+import json
 
 def index(request):
     return render(request, "app_sw/index.html")
@@ -14,15 +14,54 @@ def get_planets(request):
 
     if response.status_code == 200:
         data = response.json()
-        planets = {id: planet["name"] for id, planet in enumerate(data["results"], start=1)}
         context = {
             "planets": data["results"],
         }   
         return render(request, "app_sw/planets.html", context)
+    
 
+def planet_detail(request, id):
+    url = f"https://swapi.dev/api/planets/{id}" 
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        context = {
+            "data": data,
+        }   
+        return render(request, "app_sw/planet_detail.html", context)
 
 def get_people(request):
-    return render(request, "app_sw/people.html")
+    url = "https://swapi.dev/api/planets/"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        context = {
+            "people": data["results"],
+        }   
+        return render(request, "app_sw/people.html", context)
+
+def people_detail(request, id):
+    ...
 
 def get_starships(request):
-    return render(request, "app_sw/starships.html")
+    url = "https://swapi.dev/api/starships/"
+    url2 = "https://swapi.dev/api/starships/?page=2"
+
+    response = requests.get(url)
+    response2 = requests.get(url2)
+
+    if response.status_code == 200:
+        data = response.json()["results"]
+        data2 = response2.json()["results"]
+        data.extend(data2)
+
+        context = {
+            "starships": data,
+        }   
+        return render(request, "app_sw/starships.html", context)
+
+def starship_detail(request, id):
+    ...
